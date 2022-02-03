32338f3214d196f0476f74453f9e906c3ee4aac4
DLmar <dlmar2code@gmail.com>
2022-02-04 00:46:50 +0200

    xcs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import {useReducer} from "react";
+import React from 'react';
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'inc':
-            return {...state, count1: state.count1 + 1}
-        case 'dec':
-            return {...state, count1: state.count1 - 1}
-        case 'reset':
-            return {...state, count1: 0}
-        default:
-            throw new Error('MyError')
-    }
-}
-
-function App() {
-    const [state, dispatch] = useReducer(reducer, {count1: 0});
-
+const App = () => {
     return (
         <div>
-            <div>{state.count1}</div>
-            <button onClick={() => dispatch({type: 'inc'})}>INC</button>
-            <button onClick={() => dispatch({type: 'dec'})}>DEC</button>
-            <button onClick={() => dispatch({type: 'reset'})}>RESET</button>
+            
         </div>
     );
-}
+};
 
 export default App;
